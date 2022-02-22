1d76736344b8c0d4b52281af4bacbac2e92eaf36
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-22 21:27:26 +0100

    Added the Main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        {/* <Main /> */}
+        <Main />
         <Footer />
       </>
     );
