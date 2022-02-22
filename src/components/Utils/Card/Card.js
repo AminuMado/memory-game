95e92f20d20ccca7f0843cd67cb2d05d304c59af
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-22 23:09:59 +0100

    added the component and styling

--- src/components/Utils/Card/Card.js
+++ src/components/Utils/Card/Card.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./Card.css";
+import data from "../data";
 class Card extends React.Component {
-  render() {}
+  render() {
+    return (
+      <div
+        className="card-container"
+        id={this.props.id}
+        onClick={(event) => {
+          this.props.handleClick(event);
+        }}
+      >
+        <img src={this.props.photo}></img>
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
 }
 export default Card;
