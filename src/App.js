ed28d3877d4898131db9456279727fce1897cf76
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-23 20:57:39 +0100

    added state and imported data from state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import data from "./components/Utils/data";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { characters: data };
+    console.log(this.state);
+  }
+
   render() {
     return (
       <>
