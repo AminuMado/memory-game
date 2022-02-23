205fb28df430c1129cb8ae1d2bfa3a9e6859e416
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-23 22:58:35 +0100

    added a component did update lifecycle method

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ class App extends React.Component {
     super(props);
     this.state = { characters: data, score: 0, hiScore: 0 };
   }
+  updateScore = () => {
+    let newScore = 0;
+    const count = this.state.characters.map((character) => {
+      if (character.clicked) {
+        newScore++;
+        this.setState({ score: newScore });
+      } else return;
+    });
+  };
+
   handleClick = (event) => {
     // This handleClick function gets the id of the element clicked the id is mapped automatically if you have an id property on a tag.
     // saving the id as a const and then we try to match the id to the corresponding character with the same id in the saved state
     // we change the characters clicked property to true, while leaving everthing else as is and save it to the clicked constant created
     // we finally set the state with the new generated clicked constant
     // we do all this using map method so as to not directly mutate state.
-
+    let newScore = 0;
     const id = event.currentTarget.id;
     const clicked = this.state.characters.map((character) => {
       if (id === character.id) {
@@ -24,6 +34,13 @@ class App extends React.Component {
     });
     this.setState({ characters: clicked });
   };
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState);
+    //Typical usage, don't forget to compare the props
+    if (this.state.characters !== prevState.characters) {
+      this.updateScore();
+    }
+  }
   render() {
     return (
       <>
